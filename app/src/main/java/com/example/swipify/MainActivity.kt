e4447d96f8e62648e0f49e91a4243e85548ba6e9
Btd20 <bergahd@gmail.com>
2023-11-30 19:05:13 +0100

    Principal components

--- app/src/main/java/com/example/swipify/MainActivity.kt
+++ app/src/main/java/com/example/swipify/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.example.swipify
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 
 class MainActivity : AppCompatActivity() {
+    private val SPLASH_TIME_OUT = 4000
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.main_page)
+        setContentView(R.layout.loading_page)
+
+        Handler().postDelayed({
+            val intent = Intent(this, MainPageActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, SPLASH_TIME_OUT.toLong())
     }
 }
