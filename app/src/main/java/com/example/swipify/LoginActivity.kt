95b59b899201319704382a8eec48d2aef05a9b6c
Eric <eric.ramos.soto@gmail.com>
2023-11-30 18:21:58 +0100

    Login&Register

---
+++ app/src/main/java/com/example/swipify/LoginActivity.kt
@@ -0,0 +1,13 @@
+package com.example.swipify
+
+import android.widget.Toast
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
+
+class LoginActivity : AppCompatActivity() {
+
+}
