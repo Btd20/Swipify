cc92e69638154e5cc10dba8f3e09efa2edbd9a2b
Eric <eric.ramos.soto@gmail.com>
2023-12-09 18:34:00 +0100

    search-myList

---
+++ app/src/main/java/com/example/swipify/MyListActivity.kt
@@ -0,0 +1,24 @@
+package com.example.swipify
+
+import android.os.Bundle
+import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toolbar
+import androidx.appcompat.app.AppCompatActivity
+
+class MyListActivity : AppCompatActivity() {
+
+    fun onEditClick() {
+        // Crear funcionalidad de edit
+        // poner codigo
+    }
+    fun onDeleteClick() {
+        // Crear funcionalidad de edtit
+        // poner codigo
+    }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.my_list)
+    }
+}
--- app/src/main/java/com/example/swipify/RegisterActivity.kt
+++ app/src/main/java/com/example/swipify/RegisterActivity.kt
@@ -9,14 +9,12 @@ import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class RegisterActivity : AppCompatActivity() {
-<<<<<<< HEAD
     fun onRegisterClick(view: View) {
         // Crea un enlace para la página de registro
         // poner codigo
-=======
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.register)
->>>>>>> ec59599635456772816d7060f07656ed6563032f
     }
 }
