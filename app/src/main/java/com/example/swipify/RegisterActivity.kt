145ec204c0a2d617028004185d763ca63237f998
unknown <eric.ramos.soto@gmail.com>
2024-01-18 19:51:45 +0100

    BDD

--- app/src/main/java/com/example/swipify/RegisterActivity.kt
+++ app/src/main/java/com/example/swipify/RegisterActivity.kt
@@ -2,15 +2,53 @@ package com.example.swipify
 
 import android.content.Intent
 import android.os.Bundle
-import android.view.View
 import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class RegisterActivity : AppCompatActivity() {
+
+    private lateinit var usernameEditText: EditText
+    private lateinit var passwordEditText: EditText
+    private lateinit var confirmPasswordEditText: EditText
+    private lateinit var registerButton: Button
+    private lateinit var loginText: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.register)
+
+        usernameEditText = findViewById(R.id.username_edit_text)
+        passwordEditText = findViewById(R.id.password_edit_text)
+        confirmPasswordEditText = findViewById(R.id.confirm_password_edit_text)
+        registerButton = findViewById(R.id.login_button2)
+        loginText = findViewById(R.id.login_text)
+
+        registerButton.setOnClickListener {
+            register()
+        }
+
+        loginText.setOnClickListener {
+            navigateToLogin()
+        }
+    }
+
+    private fun register() {
+        val username = usernameEditText.text.toString()
+        val password = passwordEditText.text.toString()
+        val confirmPassword = confirmPasswordEditText.text.toString()
+
+        // implementar la lógica de registro, verificar contraseñas coincidentes, etc.
+
+        Toast.makeText(this, "Registro exitoso para $username", Toast.LENGTH_SHORT).show()
+    }
+
+    private fun navigateToLogin() {
+        val intent = Intent(this, LoginActivity::class.java)
+        startActivity(intent)
+        // Puedes agregar finish() si no quieres que el usuario vuelva a la pantalla de registro al presionar Atrás
     }
 }
+
