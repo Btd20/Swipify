61e594b4fbf367eafb868ad698aa9bd329b56449
unknown <eric.ramos.soto@gmail.com>
2024-02-08 19:37:11 +0100

    Last Hope

--- app/src/main/java/com/example/swipify/MainActivity.kt
+++ app/src/main/java/com/example/swipify/MainActivity.kt
@@ -4,19 +4,20 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
-import android.view.Menu
-import android.view.MenuItem
 import android.view.View
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.example.swipify.R.id.recyclerTendency
+import com.example.swipify.R.id.*
 import com.example.swipify.adapter.TendencyAdapter
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+
     private val SPLASH_TIME_OUT = 4000
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.loading_page)
+        setContentView(R.layout.main_page)
         initRecyclerView()
 
         Handler().postDelayed({
@@ -24,10 +25,36 @@ class MainActivity : AppCompatActivity() {
             startActivity(intent)
             finish()
         }, SPLASH_TIME_OUT.toLong())
+
+        // Find the BottomNavigationView
+        val bottomNavigationView = findViewById<BottomNavigationView>(bottom_navigation)
+
+        // Set up a NavigationListener
+        bottomNavigationView.setOnNavigationItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.page_1 -> {
+
+                    true
+                }
+                R.id.page_2 -> {
+                    // Handle navigation to page 2
+                    true
+                }
+                R.id.page_3 -> {
+                    // Handle navigation to page 3
+                    true
+                }
+                R.id.page_4 -> {
+                    // Handle search functionality
+                    true
+                }
+                else -> false
+            }
+        }
     }
 
     private fun initRecyclerView() {
-        val recyclerView = findViewById<RecyclerView>(R.id.homelist)
+        val recyclerView = findViewById<RecyclerView>(recyclerTendency)
         recyclerView.layoutManager = LinearLayoutManager(this)
         recyclerView.adapter = TendencyAdapter(TendencyProvider.tendencyList)
     }
